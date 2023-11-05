a003ba9269b1990c53ac3fecfd530f73b251dad9
gembulman <cumaliataj@gmail.com>
2023-11-05 15:46:47 +0700

    done menyelesaikan tugas

---
+++ Util/SuccessMessage.js
@@ -0,0 +1,5 @@
+const message = {
+    status:"",
+    data :{}
+}
+module.exports = message;
---
+++ testing.sh
@@ -0,0 +1,1 @@
+ssh -i "notes-api-webserver.pem" ubuntu@ec2-54-255-176-71.ap-southeast-1.compute.amazonaws.com
